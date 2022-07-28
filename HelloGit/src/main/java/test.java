47ca3c8dc02a71f4234dccb72a1ea32055fcf8d9
yzy <123456>
2022-07-28 16:48:24 +0800

    Merge branch 'master' of D:\IJData\git_workspace with conflicts.

--- HelloGit/src/main/java/test.java
+++ HelloGit/src/main/java/test.java
@@ -2,9 +2,9 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("hello git1");
-        System.out.println("¸üÐÂ1");
-        System.out.println("´´½¨·ÖÖ§");
-        System.out.println("Ö÷¸ÉÌí¼Ó");
-        System.out.println("在线更新");
+        System.out.println("����1");
+        System.out.println("������֧");
+        System.out.println("�������");
+
     }
 }
