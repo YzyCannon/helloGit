2b05e8c91d2ba5a4752f2d60e71c090ed713c01f
YzyCannon <102296210+YzyCannon@users.noreply.github.com>
2022-07-28 16:49:53 +0800

    在线更新

--- HelloGit/src/main/java/test.java
+++ HelloGit/src/main/java/test.java
@@ -2,9 +2,9 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("hello git1");
-        System.out.println("¸üÐÂ1");
-        System.out.println("´´½¨·ÖÖ§");
-        System.out.println("Ö÷¸ÉÌí¼Ó");
+        System.out.println("更新1");
+        System.out.println("创建分支");
+        System.out.println("主干添加");
         System.out.println("在线更新");
     }
 }
